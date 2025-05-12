c38252891524cb689a5e7e6561054518bc2391eb
Muhammad Ahesham <ahteshamiqbal881@gmail.com>
2025-05-12 19:02:45 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,54 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, Response
+import openai
+from openai.error import RateLimitError
+
+"""load_dotenv()
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY" )"""
+
+app = Flask(__name__)
+
+openai.api_key = "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"
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+def generate(messages, model_type):
+    def stream():
+        try:
+            response = openai.ChatCompletion.create(
+
+                model="gpt-4o-mini",
+                messages=messages,
+                stream=True
+            )
+
+            for chunk in response:
+                content = chunk['choices'][0]['delta'].get('content', '')
+                if content:
+                    yield content
+
+        except RateLimitError:
+            yield "The server is experiencing a high volume of requests. Please try again later."
+
+    return stream()
+
+
+@app.route('/gpt4', methods=['POST'])
+def gpt4():
+    data = request.get_json()
+    messages = data.get('messages', [])
+    model_type = data.get('model_type')
+
+    assistant_response = generate(messages, model_type)
+    return Response(assistant_response, mimetype='text/event-stream')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
