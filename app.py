ae4614e0bf216e4cbda11c6e17795f5832cdb8df
Muhammad Ahesham <ahteshamiqbal881@gmail.com>
2025-05-12 23:03:41 +0500

    sexong

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def gpt4():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # 👈 important for Render
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=3000)
+
